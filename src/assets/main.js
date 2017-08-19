94dd1408bc2e5964c5a3328dbcc4a49cea533041
mango2 <mango2@users.noreply.github.com>
2017-08-19 18:48:46 +0200

    First commit

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,41 @@
 $(function() {
 
   // your code will go here
+  // https://www.codeschool.com/users/mngweb.json
 
+   $.ajax({
+    url: 'https://www.codeschool.com/users/mngweb.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+      console.log("response", response.courses.completed);
+
+      var $badges = $("#badges");
+   
+      response.courses.completed.forEach(function(course){
+        var $course = $('<div />', {
+          class: 'course'
+        }).appendTo($badges);
+
+        $('<h3 />', {
+          text: course.title
+        }).appendTo($course);        
+
+        $('<img />', {
+          src: course.badge
+        }).appendTo($course);       
+
+        $('<a />', {
+          href: course.url,
+          target: "_blank",
+          class: "btn btn-primary",
+          text: "See Course"
+        }).appendTo($course);                   
+      });
+	  
+    }
+
+  }); 
+  
+  
 });
